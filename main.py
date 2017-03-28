b5b1184368b39df249958883d184b0626ff76f70
Steve <sholtebeck@gmail.com>
2017-03-28 09:20:36 -1000

    refactored updateLastPick() function

--- main.py
+++ main.py
@@ -137,14 +137,23 @@ def nextEvent():
 
 def updateEvents():
     return
+	
+def getLastPick(event_id):
+    lastpick=memcache.get("lastpick")
+    if not lastpick:
+        picks_query = Pick.all().ancestor(event_key(event_id)).order('-pick_no')
+        pick=picks_query.fetch(1)
+		lastpick=pick.who+" picked "+pick.player
+        memcache.add("lastpick",lastpick)
+    return lastpick
     
 def updateLastPick(event,pick):
-    lastpick=memcache.get("lastpick")
+    lastpick=getLastPick(event.event_id)
     if (not lastpick or not lastpick.startswith(pick.who)):
         lastpick=pick.who+" picked "+pick.player
-        memcache.delete('lastpick')
-    else:
+    elif (not lastpick.endswith(pick.player)):
         lastpick=lastpick+" and "+pick.player
+        memcache.add("lastpick",lastpick) 
 	# send alert if needed
     pick_no = len(event.picks)+1
     event.next=event.pickers[0] if mypicks.count(pick_no)>0 else event.pickers[1]
@@ -152,9 +161,7 @@ def updateLastPick(event,pick):
         message = mail.EmailMessage(sender='admin@skipflog.appspotmail.com',subject=event.event_name)
         message.to = numbers.get(event.next)
         message.body=lastpick
-        message.send() 	
-	memcache.delete("lastpick")
-    memcache.add("lastpick",lastpick)    
+        message.send() 	 
     return
 
 class MainPage(webapp2.RequestHandler):       
